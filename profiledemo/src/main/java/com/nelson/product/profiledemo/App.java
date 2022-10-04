e1f958ee69479156fa8f993db7176c31d15c2f62
Nelson de Sousa <73802941+NelsonSSoares@users.noreply.github.com>
2022-10-04 17:21:22 -0300

    dev test prod profiles

---
+++ profiledemo/src/main/java/com/nelson/product/profiledemo/App.java
@@ -0,0 +1,13 @@
+package com.nelson.product.profiledemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
